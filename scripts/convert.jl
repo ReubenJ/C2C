68ea8171cb71a1c35ca7c4a77b9a84135f38ed9a
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-13 10:27:19 +0100

    Extra tips for step 1

--- scripts/convert.jl
+++ scripts/convert.jl
@@ -1,7 +1,11 @@
+using DrWatson
+
+@quickactivate
+
 using C2C: parse_aeon_file
 using JLD2: @save
 
 # DONE 1.1: Use parse_aeon_file(...) to parse the model in data/models
-parsed_model = parse_aeon_file("data/models/007.aeon")
+parsed_model = parse_aeon_file(datadir("models", "007.aeon"))
 # DONE 1.2: @save the result to data/parsed
-@save "data/parsed/007.jld2" parsed_model
+wsave(datadir("parsed", "007.jld2"), @strdict parsed_model)
