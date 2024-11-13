1a5e70b2df0ad46cb39670cc5627e04433e379b1
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-13 10:59:37 +0100

    Solutions for step 2

--- scripts/collect_trajectories.jl
+++ scripts/collect_trajectories.jl
@@ -5,14 +5,21 @@ using DrWatson
 using JLD2: load
 using GraphDynamicalSystems: BooleanNetworks
 using DynamicalSystems: trajectory
+using Parameters
 
 # TODO 2.1 load(...) the parsed file from data/parsed/007.jld2
-parsed_model = 
-async_bn = abn(parsed_model)
+parsed_model = load(datadir("parsed", "007.jld2"))["parsed_model"]
+async_bn = BooleanNetworks.abn(parsed_model)
 
 # TODO 2.2 collect 10 trajectories of length 100
 # hint: trajectory(abn::AsyncronousBooleanNetwork, length) gives one trajectory
-trajectories = 
+@with_kw struct TrajPara
+    n_tr=10
+    l_tr=100
+end
+params = TrajPara()
+trajectories = [trajectory(async_bn, params.l_tr) for _ in 1:params.n_tr]
 
 # TODO 2.3 Save those trajectories to data/sims/
 # How can you keep track of the parameters used?
+@tagsave(datadir("sims", savename(params, "jld2")), @strdict trajectories)
